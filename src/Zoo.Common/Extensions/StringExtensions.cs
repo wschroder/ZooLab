cdfd473b7bd47842bb7bd2f74b9d3f5f8e8378f4
Wayne Schroder <gratefulbayou@gmail.com>
2019-12-17 20:45:10 -0500

    First unit test

---
+++ src/Zoo.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Zoo.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool HasValue(this string src)
+        {
+            return !string.IsNullOrWhiteSpace(src);
+        }
+    }
+}
---
+++ test/Zoo.Test/Common/Extensions/StringExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zoo.Common.Extensions;
+
+namespace Zoo.Test.Common.Extensions
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [DataTestMethod]
+        [DataRow("a")]
+        [DataRow(".")]
+        [DataRow(". ")]
+        [DataRow(". .")]
+        [DataRow("x b c")]
+        public void HasValue_WhenHasValue(string src)
+        {
+            Assert.IsTrue(src.HasValue());
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow(" ")]
+        [DataRow(null)]
+        public void HasValue_WhenNoValue(string src)
+        {
+            Assert.IsFalse(src.HasValue());
+        }
+
+    }
+}
